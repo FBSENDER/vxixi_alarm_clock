9fa07021d158b4d4b6a228c4597698bf4bb26e2e
刘梦晨 <mengchen.liu@corp.elong.com>
2016-02-19 16:56:46 +0800

    Initial Commit

---
+++ vxixi_alarm_clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vxixi_alarm_clock
+//
+//  Created by user on 16/2/19.
+//  Copyright © 2016年 vxixi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
